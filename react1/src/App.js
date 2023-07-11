abd7639284f0e19fe3ff9b2303094d6a9f1abe43
Long Phan <longnphan@outlook.com>
2023-07-10 21:00:05 -0600

    add useEffect and useState to App.js

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,9 +1,12 @@
+import { useEffect, useState } from "react";
 import "./index.css";
 import HomePage from "./components/HomePage";
 import { data } from "./data";
 
 function App() {
+  const [employeeList, setEmployeeList] = useState([]);
 
+  useEffect(() => setEmployeeList(data), []);
 
   return <HomePage />;
 }
